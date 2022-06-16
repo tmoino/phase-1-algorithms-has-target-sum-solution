4937a0fd6ea5813db1d29bca475587e015fb3b7b
tmoino <tony.moino@student.moringaschool.com>
2022-06-16 15:53:42 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  let seenNumbers = new Set();
+  //iterating through the array
+  for (const number of array) {
+    //for the current identify a complement that adds to the target  complement = target - number
+    let complement = target - number;
+
+    //checking if their is a complement is our array
+    if (seenNumbers.has(complement)) 
+    return true;
+
+    seenNumbers.add(number);
+  }
+  return false;
 }
 
 /* 
@@ -29,6 +42,14 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([30, 9, 5, 1, 79], 31));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([22, 33, 77], 65));
 }
 
 module.exports = hasTargetSum;
